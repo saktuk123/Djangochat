0446585c4d9100146aa0144b65f753f50b2adb0f
Sakhobiddin Tukhtanazarov <saktuk12@gmail.com>
2024-06-14 20:58:34 +0200

    created index.html

--- mysite/chatapp/urls.py
+++ mysite/chatapp/urls.py
@@ -1,8 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from .import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('rooms/', include('chatapp.urls')),
+    path('', views.index, name='index'),
 ]
