f27a12373ae081ee9f3daecc1ca2ea1d1152fbcf
Sakhobiddin Tukhtanazarov <saktuk12@gmail.com>
2024-07-11 12:18:24 +0200

    created consumers.py files

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -73,6 +73,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 ASGI_APPLICATION = 'mysite.asgi.application'
 
 
+CHANNEL_LAYERS = {
+  'default': {
+    'BACKEND':'channels.layers.InMemoryChannelLayer'
+  }
+}
+
+
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
